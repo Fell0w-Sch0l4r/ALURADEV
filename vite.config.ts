b20aec1d1657be244e857ed0d9842364865351c5
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-05-30 22:25:26 +0100

    create project structure

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [
+        // Your Vite plugins here
+    ],
+    test: {
+        // Your Vitest configuration here
+        globals: true,
+        environment: "happy-dom",
+    },
+});
