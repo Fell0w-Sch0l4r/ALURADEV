165b0ffc1d780ee5ad19c966d808671ca41a4fab
Ângel Cagiza <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2024-06-07 13:20:55 +0100

    add functionality to nav

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,26 @@
 import "../css/main.css";
+
+const searchBtn = document.querySelector("#searchBtn") as HTMLButtonElement;
+const closeBtn = document.querySelector("#closeBtn") as HTMLButtonElement;
+
+searchBtn.addEventListener("click", () => {
+    document.querySelector("#btns")?.classList.add("hidden");
+    document.querySelector("#logo")?.classList.add("hidden");
+
+    document.querySelector("search")?.classList.remove("hidden");
+    document.querySelector("search")?.classList.add("flex");
+
+    document.querySelector("form")?.classList.remove("hidden");
+    closeBtn.classList.remove("hidden")
+})
+
+closeBtn.addEventListener("click", () => {
+    document.querySelector("#btns")?.classList.remove("hidden");
+    document.querySelector("#logo")?.classList.remove("hidden");
+
+    document.querySelector("search")?.classList.add("hidden");
+    document.querySelector("search")?.classList.remove("flex");
+
+    document.querySelector("form")?.classList.add("hidden");
+    closeBtn.classList.add("hidden");
+})
